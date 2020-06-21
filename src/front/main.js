9a924bac342099f32b7cfb62d7b5aa2f17f47c63
Prateek Kaushik <prateek@numeroeins.com>
2020-06-22 00:32:28 +0530

    min

--- src/front/main.js
+++ src/front/main.js
@@ -2,6 +2,7 @@ import path from 'path'
 import fs from 'fs'
 import util from 'util'
 
+
 const readdir = util.promisify(fs.readdir)
 const readFile = util.promisify(fs.readFile)
 
@@ -19,19 +20,22 @@ const appentF = (data)=>{
   })
 }
 
-
-readdir(directoryPath)
-.then((files) => {
-  files.forEach(function (file) {
-    readFile(`${ directoryPath }/${ file }`)
-    .then((str) => {
-      appentF(str)
-    })
-    .catch(err=>{
-      console.log(err)
+const concatFiles = ()=>{
+  readdir(directoryPath)
+  .then((files) => {
+    files.forEach(function (file) {
+      readFile(`${ directoryPath }/${ file }`)
+      .then((str) => {
+        appentF(str)
+      })
+      .catch(err=>{
+        console.log(err)
+      })
     })
   })
-})
-.catch(err=>{
-  console.log(err)
-})
+  .catch(err=>{
+    console.log(err)
+  })
+}
+
+export default concatFiles
