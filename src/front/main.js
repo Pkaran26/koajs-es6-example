57f439467e21b1513b341c6b56e35c5391b53ff1
Prateek Kaushik <prateek@numeroeins.com>
2020-06-22 00:20:31 +0530

    min

--- src/front/main.js
+++ src/front/main.js
@@ -7,13 +7,13 @@ const readFile = util.promisify(fs.readFile)
 
 const directoryPath = path.join(path.resolve(), '/src/front/components')
 
-fs.writeFile(`${ directoryPath }/mynewfile1.js`, '', function (err) {
+fs.writeFile(`${ path.resolve('src/public/js') }/scripts.js`, '', function (err) {
   if (err) throw err
   console.log('Saved!')
 })
 
 const appentF = (data)=>{
-  fs.appendFile(`${ directoryPath }/mynewfile1.js`, data, function (err) {
+  fs.appendFile(`${ path.resolve('src/public/js') }/scripts.js`, data, function (err) {
     if (err) throw err
     console.log('Updated!')
   })
@@ -23,7 +23,7 @@ const appentF = (data)=>{
 readdir(directoryPath)
 .then((files) => {
   files.forEach(function (file) {
-    fs.readFile(`${ directoryPath }/${ file }`)
+    readFile(`${ directoryPath }/${ file }`)
     .then((str) => {
       appentF(str)
     })
---
+++ src/public/js/scripts.js
@@ -0,0 +1,18 @@
+const About = ()=>(
+  <div class="alert alert-primary tokenPopup">
+    <strong>About</strong>
+  </div>
+)
+ReactDOM.render(<About />, document.getElementById('about'))
+const Contact = ()=>(
+  <div class="alert alert-primary tokenPopup">
+    <strong>Contact</strong>
+  </div>
+)
+ReactDOM.render(<Contact />, document.getElementById('contact'))
+const App = ()=>(
+  <div class="alert alert-primary tokenPopup">
+    <strong>App</strong>
+  </div>
+)
+ReactDOM.render(<App />, document.getElementById('app'))
